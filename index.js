da487bc850ae1458c3072d44c4e14f1c80a80bf2
Leszek Zalewski <lzalewski@babbel.com>
2021-05-18 14:59:20 +0200

    Update integration test

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ async function run() {
     core.info('Publishing build metadata ...');
 
     const payload = await generatePayload(core.getInput('slices'));
-
-    await publishPayload(
+    const result = await publishPayload(
       core.getInput('access_key_id'),
       core.getInput('secret_access_key'),
       core.getInput('meta_table_arn'),
       payload
     );
+
+    if (result['$metadata'].httpStatusCode !== 200) {
+      return core.setFailed(result);
+    }
+
+    if (process.env.DEBUG !== 'true') {
+      return;
+    }
+
+    core.debug(result.Attributes);
   } catch (error) {
     core.setFailed(error.message);
   }
